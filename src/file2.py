079ff4798572cfc011b9092eff37f2659f6bed65
DevanshRajani25 <devanshrajani25@gmail.com>
2025-05-11 20:07:39 +0530

    till autologging

---
+++ src/file2.py
@@ -0,0 +1,55 @@
+import mlflow
+import mlflow.sklearn
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+import dagshub
+
+dagshub.init(repo_owner='DevanshRajani25', repo_name='MLFlow-Practise',mlflow=True)
+
+mlflow.set_tracking_uri('https://dagshub.com/DevanshRajani25/MLflow-Practise.mlflow')
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=42)
+
+max_depth = 12
+n_estimators = 7
+
+# For new Experiment
+mlflow.set_experiment('MLflow-Exp1')
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train,y_train)
+    y_pred = rf.predict(X_test)
+
+    # Accuracy Parameter
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Confusion Matrix
+    cm = confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True,fmt='d',cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    plt.savefig('confusion-matrix.png')
+
+    # Log different parameters/artifacts/metrics
+    mlflow.log_metric('Accuracy',accuracy)
+
+    mlflow.log_param('max_depth',max_depth)
+    mlflow.log_param('n_estimators',n_estimators)
+    
+    mlflow.log_artifact('confusion-matrix.png')
+    mlflow.log_artifact(__file__)
+
+    mlflow.set_tags({'Author':'Devansh Rajani', 'Project': 'Wine Classification'})
+
+    mlflow.sklearn.log_model(rf,'Random Forest')
+    print("Accuracy",accuracy)
